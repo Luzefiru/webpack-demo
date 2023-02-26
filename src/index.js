45d716984e01df2bd9c2e9fea079a508ff2e51f0
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-26 10:21:21 +0800

    feat: add image import in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './logo.svg'
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
   
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  return element;
+}
+
+document.body.appendChild(component());
