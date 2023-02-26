7e43f504d52eb9f6b5dfd12236cf53b055d22fe9
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-26 16:18:27 +0800

    feat: finish Managing Output html file tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      // it will create its own index.html file with this title
+    }),
+ ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
