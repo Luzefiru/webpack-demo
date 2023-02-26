d4ec03a990d6a41cf462384863c684adc7938289
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-26 10:46:13 +0800

    feat: clean repo for next tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -6,28 +6,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, // select all image file extensions
-        type: 'asset/resource', // process them as an 'asset/resource'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i, // select all typeface file extensions
-        type: 'asset/resource', // process them as an 'asset/resource'
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-    ],
-  },
 };
